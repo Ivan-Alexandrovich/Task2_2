975203b45fc53695ac626910112ccac4b08f9e3c
Ivan-Alexandrovich <79168848411@yandex.ru>
2021-01-12 11:16:10 +0300

    Initial commit

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,23 @@
+package com.metanit;
+/*Задать пустой целочисленный массив размером 8 С помощью цикла заполнить его
+значениями 0 3 6 9 12 15 18 21;*/
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Insert array elements:");
+
+	int size = 8;
+	int arr[] = new int [size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = 3*i;
+    }
+
+        System.out.print("Your array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(" " + arr[i]); // Выводим на экран, полученный массив
+        }
+    }
+}
